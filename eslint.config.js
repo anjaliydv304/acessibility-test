100172d950e48b0834baf7579243a7f385365580
anjliiy@syncliner.com <anjliiy@syncliner.com>
2025-08-16 18:56:14 +0530

    testing

--- eslint.config.js
+++ eslint.config.js
@@ -7,13 +7,12 @@ export default [
   {
     ignores: ['dist'], // replaces globalIgnores
   },
+  // Instead of `extends`, spread the configs directly
+  js.configs.recommended,
+  reactHooks.configs['recommended-latest'],
+  reactRefresh.configs.vite,
   {
     files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
