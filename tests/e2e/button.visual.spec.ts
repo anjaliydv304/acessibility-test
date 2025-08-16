a2acfbd7c13ae0637fdaa428ab421bc0d35b20b9
anjliiy@syncliner.com <anjliiy@syncliner.com>
2025-08-16 19:47:57 +0530

    Add baseline snapshots for visual regression tests

--- tests/e2e/button.visual.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('Button visual regression', async ({ page }) => {
-  await page.goto('/iframe.html?id=button--primary'); // Storybook story
-  expect(await page.screenshot()).toMatchSnapshot('button-primary.png');
-});
